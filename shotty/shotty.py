8d6e06dcfba0acf71b71f06e741b47c6fdb78927
Dan Beck <dan@Dans-MacBook-Pro.local>
2018-08-13 21:29:22 -0600

    added readme info, and shotty.py file

---
+++ shotty/shotty.py
@@ -0,0 +1,10 @@
+import boto3
+
+if __name__ == 'main':
+    session = boto3.Session(profile_name='dbeck')
+    ec2 = session.resource('ec2')
+
+    for i in ec2.instances.all():
+        print(i)
+
+    
